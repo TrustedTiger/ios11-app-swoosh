0d5e14828e1f1749484d1386e6df254d1a48d7b0
Rabid Scallion <Doug@dougs-mac-mini-5.local>
2018-01-06 17:18:07 -0500

    pass data between segues

---
+++ App-Swoosh/App-Swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  App-Swoosh
+//
+//  Created by Doug Heaton on 1/6/18.
+//  Copyright © 2018 Trusted Tiger LLC. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+	var desiredLeague: String!
+	var selectedSkillLevel: String!
+	
+}
