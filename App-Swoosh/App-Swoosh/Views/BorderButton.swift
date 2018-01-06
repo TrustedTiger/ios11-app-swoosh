f88e2b788eb70e8ea0a3ee72f6c9e9a011e46b6f
Rabid Scallion <Doug@dougs-mac-mini-5.local>
2018-01-06 13:04:22 -0500

    Pushing UI layout

---
+++ App-Swoosh/App-Swoosh/Views/BorderButton.swift
@@ -0,0 +1,18 @@
+//
+//  BorderButton.swift
+//  App-Swoosh
+//
+//  Created by Doug Heaton on 1/6/18.
+//  Copyright © 2018 Trusted Tiger LLC. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		layer.borderWidth = 2.0
+		layer.borderColor = UIColor.white.cgColor
+	}
+}
